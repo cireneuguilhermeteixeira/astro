44d466be5aba278bf14047071187080c7a354356
Cireneu Araujo <cireneuguilhermeteixeira>
2025-05-30 12:00:20 -0300

    adding svelte component

---
+++ src/components/svelte/MySvelteComponent.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<h2>Hello, I'm a svelte component. Value: {count}</h2>
+<button on:click={increment}>Increase value</button>
