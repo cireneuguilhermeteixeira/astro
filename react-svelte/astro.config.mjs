9ffc45d88c99b3582775d6df1734a42a247f0e05
Cireneu Araujo <cireneuguilhermeteixeira>
2025-06-06 13:21:15 -0300

    adding ssr, ssg and isr integrations

--- react-svelte/astro.config.mjs
+++ react-svelte/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [react(), svelte()]
 });
